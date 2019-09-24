833630557cae180590577894ae2edbc03219e8f3
Aleksander Jakubowski <ol3kjak@gmail.com>
2019-09-24 15:05:08 +0200

    copy text

---
+++ src/copy.js
@@ -0,0 +1,101 @@
+const copy = {
+    English: {
+        menu: {
+            faq: 'FAQ',
+            contact: 'Contact',
+            jumper: 'Jumper',
+            timer: 'Timer',
+            home: 'Home',
+        },
+        section: {
+            counter: {
+                header: 'Check your reading speed',
+                describtion: 'Just input the text, click on start, and as soon as you are done with reading, click it again. You will see your results and progress from previous tests.',
+                inputtextdesc: 'Paste your chosen text here:',
+                timeOfReading: 'Time reading',
+                wordsPerMinute: 'Words per minute',
+                numberOfWords: 'Words in text',
+                latest: 'Latest',
+            },
+            jumper: {
+                header: 'Train your eyes',
+                describtion: 'This tool alows you to move your eyes a bit! Just input text, and follow displayed words with your eyes, without moving head. This way you\' force your muscles to work',
+            },
+            main: {
+                header: 'Read faster by making the screen do the work!',
+                describtion: 'This tool shows you text displaying it in one place, so you can eliminate jumping your eyes and loosing track of what you read, instead - focus on understanding!',
+            },
+        },
+        controls: {
+            speed: 'Speed',
+            words: 'Words',
+            rows: 'Rows',
+            stop: 'Stop',
+            start: 'Start',
+            restart: 'Restart',
+            rewind: 'Rewind',
+            reset: 'Reset',
+        },
+        faq: [
+            {
+                question: 'Why bother?',
+                answear: 'For fun, for checking your skills, and improving them. For keeping eyes sharp and maybe even just to read some text quicker.',
+            },
+            {
+                question: 'How to progress further',
+                answear: 'There are tons of useful articles available on fast reading. After that we suggest some additional training.',
+            },
+            {
+                question: 'What did you learn doing this project?',
+                answear: 'Making tools like this helps to learn new technologies, in our case it was improving RWD, Vue, Vuex skills as well as our overall project managing experience.',
+            },
+        ],
+        contact: [
+            {
+                name: 'Michał Mietliński',
+                github: 'https://github.com/michalmietlinski',
+            },
+            {
+                name: 'Aleksander Jakubowski',
+                github: 'https://github.com/ol3kjak',
+            },
+        ],
+    },
+    Polski: {
+        menu: {
+            faq: 'FAQ',
+            contact: 'Kontakt',
+            jumper: 'Skoczek !',
+            timer: 'Czas',
+            home: 'Home',
+        },
+        section: {
+            counter: {
+                header: 'Sprawdź swoje obecną predkość / tepo czytania',
+            },
+            jumper: {
+                header: 'Tranuj swój wzrok',
+                describtion: 'zabawka',
+            },
+            main: {
+                header: 'Czytaj szybciej dzięki pomocy aplikacji',
+                describtion: 'zabawka',
+            },
+        },
+        faq: [
+            {
+                question: 'Pytanie 1',
+                answear: 'Odpowiedź 1',
+            },
+            {
+                question: 'Pytanie 2',
+                answear: 'Odpowiedź 2',
+            },
+        ],
+        buttton: {
+            submit: 'SUBMIT',
+        }
+    },
+};
+
+export default copy;
