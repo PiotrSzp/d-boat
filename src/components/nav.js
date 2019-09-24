6923643ef2ba5369b0dc0c7312dde14b2e73d495
mrajewski <rajewski.ma@gmail.com>
2019-09-24 16:37:59 +0200

    nav styled

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,12 +5,11 @@ function Nav() {
         <nav className='nav'>
             <div className='wrapper nav-wrapper'>
                 <a href="/" className="nav-logo">
-                    <img src="../img/D-Boat_logo.png" alt="logo"/>
                 </a>
                 <div className="nav-menu">
                     <li className='nav-menu-el'><a href="#" className='nav-menu-link'>Model</a></li>
                     <li className='nav-menu-el'><a href="#" className='nav-menu-link'>About</a></li>
-                    <li className='nav-menu-el'><a href="#" className='nav-menu-link'>Where to buy?</a></li>
+                    <li className='nav-menu-el'><a href="#" className='nav-menu-link'>Find My dealer</a></li>
                 </div>
             </div>
         </nav>
