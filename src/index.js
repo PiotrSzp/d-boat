25d40ccee2a7b76a88b0ae3674ed764bd85541e3
mrajewski <mrajewski1@st.swps.edu.pl>
2019-09-24 11:47:03 +0200

    create-react

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss'
+
+function App(){
+    return <div>DUpa</div>
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
