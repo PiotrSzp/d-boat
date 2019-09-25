24554f5b1c6deabde6734bad39d2d4521e3d12f6
Piotr Szporko <piotr.szporko@gmail.com>
2019-09-25 19:26:13 +0200

    slider z lekkimi wodotryskami

--- src/components/Slider/SlideItem.js
+++ src/components/Slider/SlideItem.js
@@ -1,24 +1,53 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// import BoatImage from
 
 class SlideItem extends Component {
+    componentDidMount() {
+        window.setTimeout(() => {
+            const slideItem = document.getElementsByClassName('slider-item')[0];
+            const slideHeight = window.getComputedStyle(slideItem).height;
+            this.props.updateParentHeight(slideHeight);
+        }, 10);
+    }
+
+// <img
+// className='slider-img'
+//     // in copy.js only filename should be stated! src attribute adds path
+// src={ require('../../assets/slider-images/' + this.props.slide.src) }
+// alt={ this.props.slide.description }
+// />
 
 
     render() {
         return (
-            <div className='slider-item' style={this.props.active ? {} : {display: 'none'}}>
-                <img
-                    className='slider-img'
-                    /* in copy.js only filename should be stated! src attribute adds path */
+            <div className={ `slider-item ${ this.props.position }` }>
 
-                    src={require('../../assets/slider-images/' + this.props.slide.src)}
-                    alt={this.props.slide.description} />
+                <div
+                    style={ {
+                        width: '80%',
+                        paddingTop: '33%',
+                        backgroundImage: `url(${ require('../../assets/slider-images/' + this.props.slide.src) })`,
+                        backgroundSize: 'contain',
+                        backgroundPosition: 'center center',
+                        backgroundRepeat: 'no-repeat'
+                    } }
+                />
                 <div className='slider-text'>
-                    <h3>{this.props.slide.title}</h3>
-                    <p>{this.props.slide.description}</p>
+                    <h3>{ this.props.slide.title }</h3>
+                    <p>{ this.props.slide.description }</p>
                 </div>
             </div>
         );
+
     }
 }
+
+SlideItem.propTypes = {
+    slide: PropTypes.object.isRequired,
+    position: PropTypes.string.isRequired,
+    updateParentHeight: PropTypes.func.isRequired
+};
 
 export default SlideItem;
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -6,49 +6,77 @@ class Slider extends Component {
     state = {
         slides_array: [...copy.English.slider],
         activeSlide: 0,
+        prevSlide: [...copy.English.slider].length - 1,
+        nextSlide: 1,
+        sliderHeight: false,
     };
-
 
     prev_slide = e => {
         e.preventDefault();
-        this.setState({
-            activeSlide: this.state.activeSlide - 1 < 0 ? this.state.slides_array.length - 1 : this.state.activeSlide - 1
-        });
-        let newSlidesArr = this.state.slides_array.map((slide, index) => ({
-            ...slide,
-            active: index === this.state.activeSlide
+        e.stopPropagation();
+        this.setState(prevState => ({
+            activeSlide: prevState.activeSlide - 1 < 0 ? prevState.slides_array.length - 1 : prevState.activeSlide - 1,
+            prevSlide: prevState.prevSlide - 1 < 0 ? prevState.slides_array.length - 1 : prevState.prevSlide - 1,
+            nextSlide: prevState.activeSlide
         }));
-        this.setState({
-            slides_array: newSlidesArr
-        });
     };
-
 
     next_slide = e => {
         e.preventDefault();
-        this.setState({
-            activeSlide: this.state.activeSlide + 1 > this.state.slides_array.length - 1 ? 0 : this.state.activeSlide + 1
-        });
-        let newSlidesArr = this.state.slides_array.map((slide, index) => ({
-            ...slide,
-            active: index === this.state.activeSlide
+        e.stopPropagation();
+        this.setState(prevState => ({
+            activeSlide: prevState.activeSlide + 1 > prevState.slides_array.length - 1 ? 0 : prevState.activeSlide + 1,
+            prevSlide: prevState.activeSlide,
+            nextSlide: prevState.nextSlide + 1 > prevState.slides_array.length - 1 ? 0 : prevState.nextSlide + 1
         }));
-        this.setState({
-            slides_array: newSlidesArr
-        });
+    };
+
+    determinePosition = (index) => {
+        if (index === this.state.activeSlide) {
+            return 'current'
+        } else if (index === this.state.prevSlide) {
+            return 'prev'
+        } else if (index === this.state.nextSlide) {
+            return 'next'
+        } else return ''
+    };
+
+
+    updateHeight = height => {
+        this.setState({ sliderHeight: height });
     };
 
 
     render() {
-        return (<div className='slider-wrapper'>
-                <div className='slider-nav left'>
-                    <button className='slider-button prev' id='slider-button-left' onClick={ this.prev_slide }>LEWO</button>
+        const { activeSlide, nextSlide, prevSlide } = this.state;
+
+        return (
+            <div
+                className='slider-wrapper'
+                style={ { height: `${ this.state.sliderHeight }` } }>
+                <div className='slider-nav prev' onClick={ this.prev_slide }>
+                    <button
+                        className='slider-button prev'
+                        id='slider-button-left'
+                        onClick={ this.prev_slide } />
                 </div>
-                <div className='slider-nav right'>
-                    <button className='slider-button next' id='slider-button-right' onClick={ this.next_slide }>PRAWO</button>
+                <div className='slider-nav next' onClick={ this.next_slide }>
+                    <button
+                        className='slider-button next'
+                        id='slider-button-right'
+                        onClick={ this.next_slide } />
                 </div>
                 {
-                    this.state.slides_array.map((slide, index) => <SlideItem key={ slide.id } slide={ slide } active={ index === this.state.activeSlide }/>
+                    this.state.slides_array.map((slide, index) => {
+                            if (index === activeSlide || index === nextSlide || index === prevSlide) {
+                                return <SlideItem
+                                    key={ slide.id }
+                                    slide={ slide }
+                                    position={ this.determinePosition(index) }
+                                    updateParentHeight={ this.updateHeight }
+                                />
+                            } else return null;
+                        }
                     )
                 }
             </div>
