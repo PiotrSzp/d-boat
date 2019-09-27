2b6966cc31fdb2072c4d038a2de8a6cc52f1720f
mrajewski <rajewski.ma@gmail.com>
2019-09-27 18:39:23 +0200

    components fix

--- src/components/VideoStart/Container.js
+++ src/components/VideoStart/Container.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Video from "./Video";
 import video from "../../assets/movie/IntroConv.mp4";
 import copy from "../../copy";
