bd1f06a07c564739aad93b2ce5a9790022b4b3ad
mrajewski <rajewski.ma@gmail.com>
2019-10-03 15:41:09 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -16,8 +16,6 @@ function App() {
     return (
         <Router>
             <Nav />
-            
-        </>
             <Switch>
                 <Route exact path='/'>
                     <IntroPage />
