ecd7994e4969dbe182700553f8f812bd1cdc2021
Piotr Szporko <piotr.szporko@gmail.com>
2019-10-17 19:26:15 +0200

    Beginning of dropdown menu feature

--- src/components/Navigation/BurgerMenu.js
+++ src/components/Navigation/BurgerMenu.js
@@ -1,41 +1,55 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
-
 class Burger extends Component {
-
-    state={
-      isOn: false
+    state = {
+        isOn: false
     };
 
-    handleOnClick = ()=>{
-      this.setState(prev=>({
-          isOn: !prev.isOn
-      }))
+    handleOnClick = () => {
+        this.setState(prev => ({
+            isOn: !prev.isOn
+        }))
     };
-    handleOnLink = ()=>{
-        this.setState(prev=>({
+
+    handleOnLink = () => {
+        this.setState(prev => ({
             isOn: false
         }))
     };
+
     render() {
         return (
             <>
-            {/*Burger*/}
-            <button onClick={this.handleOnClick} className={`burger-menu ${this.state.isOn?'burger-active':null}`}>
-                <div className='burger-line'></div>
-                <div className='burger-line'></div>
-                <div className='burger-line'></div>
-            </button>
-
-                {/*//Slide Menu*/}
-        {this.state.isOn?<ul className='burger-list'>{this.props.list.map(el=>{
-            return  <li onClick={this.handleOnLink} key={el.id} className='burger-el'><NavLink to={el.link} className='burger-link'>{el.text}</NavLink></li>
-        })}</ul>:null}
-        </>
+                {/*Burger*/ }
+                <button onClick={ this.handleOnClick }
+                        className={ `burger-menu ${ this.state.isOn ? 'burger-active' : null }` }>
+                    <div className='burger-line' />
+                    <div className='burger-line' />
+                    <div className='burger-line' />
+                </button>
+                {/*Slide Menu*/ }
+                { this.state.isOn ? <ul className='burger-list'>
+                    { this.props.list.map(el => {
+                        return (
+                            <li
+                                onClick={ this.handleOnLink }
+                                key={ el.id }
+                                className='burger-el'
+                            >
+                                <NavLink
+                                    to={ el.link }
+                                    className='burger-link'
+                                >
+                                    { el.text }
+                                </NavLink>
+                            </li>
+                        )
+                    }) }</ul> : null
+                }
+            </>
         )
     }
-
 }
 
 export default Burger;
--- src/components/Navigation/NavMenu.js
+++ src/components/Navigation/NavMenu.js
@@ -1,17 +1,62 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import copy from "../../copy";
+import SlideToggle from 'react-slide-toggle';
 
 function NavMenu() {
     return (
         <ul className="nav-menu">
-            {copy.English.menu.map((el, i) => {
-                return <li key={el.id} className='nav-menu-el'><NavLink to={el.link} className='nav-menu-link'>{el.text}</NavLink>
-                </li>
-            })}
+            { copy.English.menu.map(el => {
+                return <SlideToggle
+                    key={ el.id }
+                    duration={ 500 }
+                    collapsed
+                    render={ ({ toggle, setCollapsibleElement }) => (
+                        <li
+                            className='nav-menu-el'
+                            onClick={ toggle }
+                        >
+                            <NavLink
+                                to={ el.link }
+                                className='nav-menu-link'
+                            >
+                                { el.text }
+                            </NavLink>
+                            {
+                                el.submenu ? <ul
+                                    ref={ setCollapsibleElement }
+                                    className='nav-menu-submenu'
+                                >
+                                    {
+                                        el.submenu.map(subEl => {
+                                            return (
+                                                <li
+                                                    key={ subEl.id }
+                                                    className='nav-menu-subel'
+                                                >
+                                                    <NavLink
+                                                        to={ subEl.link }
+                                                        className='nav-menu-sublink'
+                                                        style={ {
+                                                            // zeby to działało, chyba trzeba zamknąć cały element menu w komponencie i przekazać ścieżkę do propsa
+                                                            backgroundImage: `url(${ require('../../assets/slider-images/sample-boat-slide.png') })`,
+                                                            // backgroundImage: `url(${  })`,
+                                                        } }
+                                                    >
+                                                        { subEl.text }
+                                                    </NavLink>
+                                                </li>
+                                            )S
+                                        })
+                                    }
+                                </ul> : null
+                            }
+                        </li>
+                    ) }
+                />
+            }) }
         </ul>
     )
 }
-
 
 export default NavMenu
