96a1dd78a04018486cd234e7824eeabe101fa7fe
Piotr Szporko <piotr.szporko@gmail.com>
2019-12-05 21:08:44 +0100

    Working carousel, no CMS

--- src/pages/Model.js
+++ src/pages/Model.js
@@ -90,7 +90,7 @@ class Model extends Component {
         })
         .then(() => {
             this.setState(state => {
-                const videoSrc = require('../assets/movie/' + state.model.videoFileName);
+                const videoSrc = require(`../assets/movie/${state.model.videoFileName}`);
                 return { videoSrc };
             })
         })
